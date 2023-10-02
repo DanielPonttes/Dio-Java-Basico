f75cac63019058de8fed26d905502af269a602e1
DanielPonttes <143747943+DanielPonttes@users.noreply.github.com>
2023-10-02 00:14:49 -0300

    ControleFluxo
    Desafio contendo o básico sobre controle de fluxo, com um exercício tendo um for, if e exception sendo usados de maneira prática e útil.

---
+++ DesafioControleFluxo/src/Desafio/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Desafio;
+
+public class ParametrosInvalidosException extends Exception {
+	public static void main(String[] args) {
+	System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+	}
+}
